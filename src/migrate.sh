3000aebab414a8c27effb6089476a4ef64615262
Tatsuya28 <ioiodauphin880@gmail.com>
2024-12-01 13:10:59 -0500

    Ajout d'une attente pour la disponibilité de MySQL avant d'appliquer les migrations et de créer le superutilisateur

--- src/migrate.sh
+++ src/migrate.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+set -e
+
+# Paramètres
+DB_HOST=${MYSQL_HOST}
+DB_PORT=${MYSQL_PORT}
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"theai00tester@gmail.com"}
+
 cd /app/
 
-/opt/venv/bin/python manage.py migrate --noinput # Apply database migrations
-/opt/venv/bin/python manage.py createsuperuser --noinput --email $SUPERUSER_EMAIL || true # Create superuser if not exists
+# Attente de la disponibilité de MySQL
+echo "Waiting for MySQL at $DB_HOST..."
+until nc -z "$DB_HOST" $DB_PORT; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - proceeding"
+
+# Appliquer les migrations
+echo "Applying Django migrations..."
+# /opt/venv/bin/python manage.py makemigrations --noinput
+/opt/venv/bin/python manage.py migrate --noinput
+
+# Créer le superutilisateur si nécessaire
+echo "Creating superuser if necessary..."
+/opt/venv/bin/python manage.py createsuperuser --noinput --email $SUPERUSER_EMAIL || true
