d1bd65dc15620b41faf4bc477c01cab78620de35
thomasfridblatt <95437533+thomasfridblatt@users.noreply.github.com>
2024-12-05 19:05:26 -0500

    Modification pour l'ajout de la base de données des tests

--- src/init.sql
+++ src/init.sql
@@ -2,3 +2,8 @@ CREATE DATABASE IF NOT EXISTS django_cicd_db;
 CREATE USER IF NOT EXISTS 'myuser'@'%' IDENTIFIED BY 'mypassword';
 GRANT ALL PRIVILEGES ON django_cicd_db.* TO 'myuser'@'%';
 FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS test_django_cicd_db;
+CREATE USER IF NOT EXISTS 'myuser'@'%' IDENTIFIED BY 'mypassword';
+GRANT ALL PRIVILEGES ON test_django_cicd_db.* TO 'myuser'@'%';
+FLUSH PRIVILEGES;
